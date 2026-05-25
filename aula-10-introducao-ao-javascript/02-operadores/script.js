ff9a123ebdcb1fc71b2921ab6b45e6746e923dbf
DSM1 <fatec-dsm1@academico.intra>
2026-05-25 19:27:31 -0300

    JavaScript

---
+++ aula-10-introducao-ao-javascript/02-operadores/script.js
@@ -0,0 +1,39 @@
+// JAVASCRIPT
+
+let n1 = Number(window.prompt(`Digite um número:`))
+let n2 = Number(window.prompt(`Digite outro número:`))
+const soma = n1 + n2
+
+window.alert(`A soma dos valores é ${soma}`)
+
+// DEMAIS OPERADORES
+
+// MULTIPLICAÇÃO
+let mult = n1 * n2
+document.writeln(`<br>A multiplicação de ${n1} e ${n2} é igual a ${mult}.`)
+
+// SUBTRAÇÃO
+let sub = n1 - n2
+document.writeln(`<br>A subtração de ${n1} e ${n2} é igual a ${sub}.`)
+
+// POTÊNCIA
+let pot = n1 ** n2
+document.writeln(`<br>A potência de ${n1} elevado a ${n2} é igual a ${sub}.`)
+
+// DIVISÃO REAL
+let divr = n1 / n2
+document.writeln(`<br>A divisão de ${n1} por ${n2} é igual a ${divr}.`)
+
+// RESTO DE DIVISÃO
+let rdiv = n1 % n2
+document.writeln(`<br>O resto da divisão de ${n1} por ${n2} é igual a ${rdiv}.`)
+
+// OPERADOR DE ATRIBUIÇÃO
+n1 += n2
+document.writeln(`<br>${n1}`)
+n1 += n2
+document.writeln(`<br>${n1}`)
+n1 += n2
+document.writeln(`<br>${n1}`)
+
+
